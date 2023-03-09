0aa275b76a6e4c17c1dd8731a2160a95a3e981b8
Benjamin Ross <benr22@gmail.com>
2023-03-09 09:13:22 -0700

    working on event loop

--- broadcast/node/broadcast.go
+++ broadcast/node/broadcast.go
@@ -3,7 +3,6 @@ package node
 import (
 	"encoding/json"
 	"log"
-	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -19,7 +18,12 @@ type Node struct {
 	messages  []int
 	seen      map[int]struct{}
 	neighbors []string
-	seenLock  sync.RWMutex
+	msgs      chan *internalMessage
+}
+
+type internalMessage struct {
+	t   string
+	msg maelstrom.Message
 }
 
 type BroadcastMessage struct {
@@ -43,33 +47,18 @@ func NewBroadcast() *Node {
 		messages:  make([]int, 0),
 		seen:      make(map[int]struct{}, 0),
 		neighbors: make([]string, 0),
+		msgs:      make(chan *internalMessage),
 	}
 }
 
 func (bn *Node) Register() {
 	bn.n.Handle("broadcast", func(msg maelstrom.Message) error {
-		body := new(BroadcastMessage)
-		if err := json.Unmarshal(msg.Body, body); err != nil {
-			return err
+		im := &internalMessage{
+			t:   "broadcast",
+			msg: msg,
 		}
-
-		if body.Path == nil {
-			log.Printf("Path was empty")
-			body.Path = make(map[string]struct{})
-		} else {
-			log.Printf("Path: %v\n", body.Path)
-		}
-
-		body.Path[msg.Src] = struct{}{}
-
-		if val := bn.addNewMessage(body.Message); val == ADDED {
-			//bn.sendToNeighbors(body.Message, body.Path)
-		}
-
-		resp := make(map[string]string)
-		resp["type"] = "broadcast_ok"
-
-		return bn.n.Reply(msg, resp)
+		bn.msgs <- im
+		return nil
 	})
 
 	bn.n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
@@ -77,67 +66,122 @@ func (bn *Node) Register() {
 	})
 
 	bn.n.Handle("read", func(msg maelstrom.Message) error {
-		resp := make(map[string]any)
-		resp["type"] = "read_ok"
-		bn.seenLock.RLock()
-		resp["messages"] = bn.messages
-		bn.seenLock.RUnlock()
-
-		return bn.n.Reply(msg, resp)
+		im := &internalMessage{
+			t:   "read",
+			msg: msg,
+		}
+		bn.msgs <- im
+		return nil
 	})
 
 	bn.n.Handle("topology", func(msg maelstrom.Message) error {
-		body := new(TopologyMessage)
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
+		im := &internalMessage{
+			t:   "topology",
+			msg: msg,
 		}
-
-		// Create spanning tree of the topology
-		// start with n0, since we know there will always be
-		// at least one node
-		start := "n0"
-		queue := []string{start}
-		spanningTree := make(map[string][]string)
-		visited := make(map[string]struct{})
-		visited[start] = struct{}{}
-
-		for len(queue) > 0 {
-			var n string
-			n, queue = queue[0], queue[1:]
-			for _, neighbor := range body.Topology[n] {
-				if _, ok := visited[neighbor]; !ok {
-					visited[neighbor] = struct{}{}
-					queue = append(queue, neighbor)
-					spanningTree[n] = append(spanningTree[n], neighbor)
-					spanningTree[neighbor] = append(spanningTree[neighbor], n)
-				}
-			}
-		}
-
-		log.Printf("spanning tree: %v\n", spanningTree)
-		bn.neighbors = spanningTree[bn.n.ID()]
-
-		resp := make(map[string]string)
-		resp["type"] = "topology_ok"
-
-		return bn.n.Reply(msg, resp)
+		bn.msgs <- im
+		return nil
 	})
 }
 
 func (bn *Node) Run() error {
-	ticker := time.NewTicker(100 * time.Millisecond)
 	go func() {
+		ticker := time.NewTicker(100 * time.Millisecond)
 		for {
 			select {
+			case m := <-bn.msgs:
+				switch m.t {
+				case "broadcast":
+					if err := bn.handleBroadcast(m.msg); err != nil {
+						log.Fatalf("error handling broadcast")
+					}
+				case "read":
+					if err := bn.handleRead(m.msg); err != nil {
+						log.Fatalf("error handling read")
+					}
+				case "topology":
+					if err := bn.handleTopology(m.msg); err != nil {
+						log.Fatalf("error handling topology")
+					}
+				}
 			case <-ticker.C:
 				bn.reconcile()
+			default:
 			}
 		}
 	}()
 
 	return bn.n.Run()
 }
+func (bn *Node) handleBroadcast(msg maelstrom.Message) error {
+	body := new(BroadcastMessage)
+	if err := json.Unmarshal(msg.Body, body); err != nil {
+		return err
+	}
 
+	if body.Path == nil {
+		log.Printf("Path was empty")
+		body.Path = make(map[string]struct{})
+	} else {
+		log.Printf("Path: %v\n", body.Path)
+	}
+
+	body.Path[msg.Src] = struct{}{}
+
+	if val := bn.addNewMessage(body.Message); val == ADDED {
+		//bn.sendToNeighbors(body.Message, body.Path)
+	}
+
+	resp := make(map[string]string)
+	resp["type"] = "broadcast_ok"
+
+	return bn.n.Reply(msg, resp)
+}
+
+func (bn *Node) handleRead(msg maelstrom.Message) error {
+	resp := make(map[string]any)
+	resp["type"] = "read_ok"
+	resp["messages"] = bn.messages
+
+	return bn.n.Reply(msg, resp)
+}
+
+func (bn *Node) handleTopology(msg maelstrom.Message) error {
+	body := new(TopologyMessage)
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	// Create spanning tree of the topology
+	// start with n0, since we know there will always be
+	// at least one node
+	start := "n0"
+	queue := []string{start}
+	spanningTree := make(map[string][]string)
+	visited := make(map[string]struct{})
+	visited[start] = struct{}{}
+
+	for len(queue) > 0 {
+		var n string
+		n, queue = queue[0], queue[1:]
+		for _, neighbor := range body.Topology[n] {
+			if _, ok := visited[neighbor]; !ok {
+				visited[neighbor] = struct{}{}
+				queue = append(queue, neighbor)
+				spanningTree[n] = append(spanningTree[n], neighbor)
+				spanningTree[neighbor] = append(spanningTree[neighbor], n)
+			}
+		}
+	}
+
+	log.Printf("spanning tree: %v\n", spanningTree)
+	bn.neighbors = spanningTree[bn.n.ID()]
+
+	resp := make(map[string]string)
+	resp["type"] = "topology_ok"
+
+	return bn.n.Reply(msg, resp)
+}
 func (bn *Node) sendToNeighbors(m int, path map[string]struct{}) {
 	for _, neighbor := range bn.neighbors {
 		if _, ok := path[neighbor]; ok {
@@ -159,8 +203,6 @@ func (bn *Node) send(neighbor string, message int, path map[string]struct{}) err
 }
 
 func (bn *Node) addNewMessage(m int) int {
-	bn.seenLock.Lock()
-	defer bn.seenLock.Unlock()
 	if _, ok := bn.seen[m]; ok {
 		return SEEN
 	}
@@ -175,21 +217,9 @@ func (bn *Node) readOk(msg maelstrom.Message) error {
 		return err
 	}
 
-	neighborSeen := make(map[int]struct{})
-
-	log.Printf("Messages received: %v\n", body.Messages)
 	for _, message := range body.Messages {
-		neighborSeen[message] = struct{}{}
-		if val := bn.addNewMessage(message); val == ADDED {
-			log.Printf("message %d added\n", message)
-		}
+		bn.addNewMessage(message)
 	}
-
-	// 	path := make(map[string]struct{})
-	// 	path[bn.n.ID()] = struct{}{}
-	// 	for _, message := range bn.messages {
-	// 		bn.send(msg.Src, message, path)
-	// 	}
 
 	return nil
 }
@@ -199,7 +229,6 @@ func (bn *Node) reconcile() {
 	req["type"] = "read"
 
 	for _, neighbor := range bn.neighbors {
-		log.Printf("reconciling with neighbor: %s\n", neighbor)
 		err := bn.n.RPC(neighbor, req, bn.readOk)
 		if err != nil {
 			log.Fatalf("FAILED TO READ FROM NEIGHBOR: %s\n", neighbor)
